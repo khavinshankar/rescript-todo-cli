51d55f8a048bc5999c2236c6c984e8cbff701215
khavinshankar <khavinshankar@gmail.com>
2021-02-07 10:36:59 +0530

    todo cli in rescript

--- src/todo.bs.js
+++ src/todo.bs.js
@@ -4,6 +4,9 @@
 var Fs = require("fs");
 var Os = require("os");
 var Curry = require("bs-platform/lib/js/curry.js");
+var Belt_Int = require("bs-platform/lib/js/belt_Int.js");
+var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
+var Caml_array = require("bs-platform/lib/js/caml_array.js");
 
 var getToday = (function() {
   let date = new Date();
@@ -14,18 +17,176 @@ var getToday = (function() {
 
 var encoding = "utf8";
 
-console.log("Hello! today is " + Curry._1(getToday, undefined));
+var todoFile = "todo.txt";
 
-if (Fs.existsSync("todo.txt")) {
-  console.log("Todo file exists.");
-} else {
-  Fs.writeFileSync("todo.txt", "This is todo!" + Os.EOL, {
+var doneFile = "done.txt";
+
+var helpInfo = "Usage :-\n$ ./todo add \"todo item\"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics";
+
+function arrlen(arr) {
+  return arr.length;
+}
+
+function strlen(str) {
+  return str.length;
+}
+
+function strToInt(str) {
+  var value = Belt_Int.fromString(str);
+  if (value !== undefined) {
+    return value;
+  } else {
+    return -1;
+  }
+}
+
+function fileRead(filename) {
+  if (!Fs.existsSync(filename)) {
+    return [];
+  }
+  var filedata = Fs.readFileSync(filename, {
         encoding: encoding,
-        flag: "w"
+        flag: "r"
       });
-  console.log("Todo file created.");
+  return filedata.trim().split(Os.EOL);
+}
+
+function fileWrite(filename, todos) {
+  var content = Belt_Array.joinWith(todos, Os.EOL, (function (todo) {
+          return todo;
+        }));
+  Fs.writeFileSync(filename, content, {
+        encoding: encoding,
+        flag: "w+"
+      });
+  
+}
+
+function fileAppend(filename, todo) {
+  var content = todo + Os.EOL;
+  Fs.appendFileSync(filename, content, {
+        encoding: encoding,
+        flag: "a+"
+      });
+  
+}
+
+var args = Belt_Array.slice(process.argv, 2, process.argv.length);
+
+function help(param) {
+  console.log(helpInfo);
+  
+}
+
+function add(param) {
+  if (args.length === 1) {
+    console.log("Error: Missing todo string. Nothing added!");
+  } else {
+    fileAppend(todoFile, Caml_array.get(args, 1));
+    console.log("Added todo: \"" + Caml_array.get(args, 1) + "\"");
+  }
+  
+}
+
+function ls(param) {
+  var todos = fileRead(todoFile);
+  if (todos.length === 0) {
+    console.log("There are no pending todos!");
+    return ;
+  } else {
+    return Belt_Array.forEachWithIndex(Belt_Array.reverse(todos), (function (i, todo) {
+                  console.log("[" + String(todos.length - i | 0) + "] " + todo);
+                  
+                }));
+  }
+}
+
+function del(param) {
+  if (args.length === 1) {
+    console.log("Error: Missing NUMBER for deleting todo.");
+    return ;
+  }
+  var todos = fileRead(todoFile);
+  var pos = strToInt(Caml_array.get(args, 1));
+  var idx = todos.length - pos | 0;
+  if (todos.length <= idx || idx < 0) {
+    console.log("Error: todo #" + String(pos) + " does not exist. Nothing deleted.");
+  } else {
+    todos.splice(idx, 1);
+    fileWrite(todoFile, todos);
+    console.log("Deleted todo #" + String(pos));
+  }
+  
+}
+
+function don(param) {
+  if (args.length === 1) {
+    console.log("Error: Missing NUMBER for marking todo as done.");
+    return ;
+  }
+  var todos = fileRead(todoFile);
+  var pos = strToInt(Caml_array.get(args, 1));
+  var idx = todos.length - pos | 0;
+  if (todos.length <= idx || idx < 0) {
+    console.log("Error: todo #" + String(pos) + " does not exist.");
+    return ;
+  }
+  var completed = todos.splice(idx, 1);
+  fileWrite(todoFile, todos);
+  var completedTodo = "x " + Curry._1(getToday, undefined) + " " + Caml_array.get(completed, 0);
+  fileAppend(doneFile, completedTodo);
+  console.log("Marked todo #" + String(pos) + " as done.");
+  
+}
+
+function rep(param) {
+  var active = fileRead(todoFile);
+  var completed = fileRead(doneFile);
+  console.log(Curry._1(getToday, undefined) + " Pending : " + String(active.length) + " Completed : " + String(completed.length));
+  
+}
+
+if (args.length === 0) {
+  console.log(helpInfo);
+} else {
+  var match = Caml_array.get(args, 0);
+  switch (match) {
+    case "add" :
+        add(undefined);
+        break;
+    case "del" :
+        del(undefined);
+        break;
+    case "done" :
+        don(undefined);
+        break;
+    case "ls" :
+        ls(undefined);
+        break;
+    case "report" :
+        rep(undefined);
+        break;
+    default:
+      console.log(helpInfo);
+  }
 }
 
 exports.getToday = getToday;
 exports.encoding = encoding;
-/*  Not a pure module */
+exports.todoFile = todoFile;
+exports.doneFile = doneFile;
+exports.helpInfo = helpInfo;
+exports.arrlen = arrlen;
+exports.strlen = strlen;
+exports.strToInt = strToInt;
+exports.fileRead = fileRead;
+exports.fileWrite = fileWrite;
+exports.fileAppend = fileAppend;
+exports.args = args;
+exports.help = help;
+exports.add = add;
+exports.ls = ls;
+exports.del = del;
+exports.don = don;
+exports.rep = rep;
+/* args Not a pure module */
--- src/todo.res
+++ src/todo.res
@@ -39,11 +39,137 @@ NOTE: The code below is provided just to show you how to use the
 date and file functions defined above. Remove it to begin your implementation.
 */
 
-Js.log("Hello! today is " ++ getToday())
+/* * Constant Values */
+@bs.val external __dirname: string = "__dirname"
+@bs.val external argv: array<string> = "process.argv"
 
-if existsSync("todo.txt") {
-  Js.log("Todo file exists.")
+let todoFile = "todo.txt"
+let doneFile = "done.txt"
+
+let helpInfo = `Usage :-
+$ ./todo add "todo item"  # Add a new todo
+$ ./todo ls               # Show remaining todos
+$ ./todo del NUMBER       # Delete a todo
+$ ./todo done NUMBER      # Complete a todo
+$ ./todo help             # Show usage
+$ ./todo report           # Statistics`
+
+/* * Helper Functions */
+let arrlen = arr => arr->Belt.Array.length
+let strlen = str => str->Js.String.length
+
+let strToInt = str => {
+  let value = Belt.Int.fromString(str)
+  switch value {
+  | None => -1
+  | Some(x) => x
+  }
+}
+
+let fileRead = filename => {
+  if filename->existsSync {
+    let filedata = filename->readFileSync({encoding: encoding, flag: "r"})
+    eol->Js.String.split(filedata->Js.String.trim)
+  } else {
+    []
+  }
+}
+
+let fileWrite = (filename, todos) => {
+  let content = todos->Belt.Array.joinWith(eol, todo => todo)
+  filename->writeFileSync(content, {encoding: encoding, flag: "w+"})
+}
+
+let fileAppend = (filename, todo) => {
+  let content = todo ++ eol
+  filename->appendFileSync(content, {encoding: encoding, flag: "a+"})
+}
+
+/* * Program Code */
+let args = argv->Belt.Array.slice(~offset=2, ~len=argv->arrlen)
+
+let help = () => {
+  Js.log(helpInfo)
+}
+
+let add = () => {
+  if args->arrlen == 1 {
+    Js.log("Error: Missing todo string. Nothing added!")
+  } else {
+    todoFile->fileAppend(args[1])
+    Js.log(`Added todo: "${args[1]}"`)
+  }
+}
+
+let ls = () => {
+  let todos = todoFile->fileRead
+  if todos->arrlen == 0 {
+    Js.log("There are no pending todos!")
+  } else {
+    todos
+    ->Belt.Array.reverse
+    ->Belt.Array.forEachWithIndex((i, todo) =>
+      Js.log(`[${Belt.Int.toString(todos->arrlen - i)}] ${todo}`)
+    )
+  }
+}
+
+let del = () => {
+  if args->arrlen == 1 {
+    Js.log("Error: Missing NUMBER for deleting todo.")
+  } else {
+    let todos = todoFile->fileRead
+    let pos = strToInt(args[1])
+    let idx = todos->arrlen - pos
+    if todos->arrlen <= idx || idx < 0 {
+      Js.log(`Error: todo #${Belt.Int.toString(pos)} does not exist. Nothing deleted.`)
+    } else {
+      let _ = todos->Js.Array.spliceInPlace(~pos=idx, ~remove=1, ~add=[])
+      todoFile->fileWrite(todos)
+      Js.log(`Deleted todo #${Belt.Int.toString(pos)}`)
+    }
+  }
+}
+
+let don = () => {
+  if args->arrlen == 1 {
+    Js.log("Error: Missing NUMBER for marking todo as done.")
+  } else {
+    let todos = todoFile->fileRead
+    let pos = strToInt(args[1])
+    let idx = todos->arrlen - pos
+    if todos->arrlen <= idx || idx < 0 {
+      Js.log(`Error: todo #${Belt.Int.toString(pos)} does not exist.`)
+    } else {
+      let completed = todos->Js.Array.spliceInPlace(~pos=idx, ~remove=1, ~add=[])
+      todoFile->fileWrite(todos)
+      let completedTodo = `x ${getToday()} ${completed[0]}`
+      doneFile->fileAppend(completedTodo)
+      Js.log(`Marked todo #${Belt.Int.toString(pos)} as done.`)
+    }
+  }
+}
+
+let rep = () => {
+  let active = todoFile->fileRead
+  let completed = doneFile->fileRead
+
+  Js.log(
+    `${getToday()} Pending : ${Belt.Int.toString(active->arrlen)} Completed : ${Belt.Int.toString(
+        completed->arrlen,
+      )}`,
+  )
+}
+
+if args->arrlen == 0 {
+  help()
 } else {
-  writeFileSync("todo.txt", "This is todo!" ++ eol, {encoding: encoding, flag: "w"})
-  Js.log("Todo file created.")
+  switch args[0] {
+  | "add" => add()
+  | "ls" => ls()
+  | "del" => del()
+  | "done" => don()
+  | "report" => rep()
+  | _ => help()
+  }
 }
